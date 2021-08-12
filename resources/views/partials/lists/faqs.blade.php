3230054c08202bd205698fbea34dd88c2dc8ccc0
Alin Ivan <ivana@roehampton.ac.uk>
2021-08-12 17:34:59 +0100

    AI: Added storage to verioning

--- resources/views/partials/lists/faqs.blade.php
+++ resources/views/partials/lists/faqs.blade.php
@@ -3,12 +3,12 @@
                         @foreach(F1::getDataOfModel('faq') as $row)
                         <div class="card">
                             <div class="card-header" id="headingOne">
-                                <h4 class="mb-0" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                <h4 class="mb-0" data-toggle="collapse" data-target="#collapsefaq{{$row->id}}" aria-expanded="true" aria-controls="collapseOne">
                                     {{$row->question}}
                                 </h4>
                             </div>
 
-                            <div id="collapseOne" class="collapse {{$loop->first ? 'show': ''}}" aria-labelledby="headingOne" data-parent="#accordionExample">
+                            <div id="collapsefaq{{$row->id}}" class="collapse {{$loop->first ? 'show': ''}}" aria-labelledby="headingOne" data-parent="#accordionExample">
                                 <div class="card-body" style="text-align:justify">
                                     {!! $row->answer !!}                         
                                 </div>
